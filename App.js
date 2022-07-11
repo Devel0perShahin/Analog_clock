0552d8c04ab15fc483ca278808b02c0cb0317242
shahinul islam <shahinulislampatoary@gmail.com>
2022-07-11 13:11:44 +0600

    make full dynamic for any size

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ let Hours_hand = document.querySelector(".Hours");
 
 setInterval(Clock_fn, 1000);
 
-function Clock_fn(){
+function Clock_fn() {
     Time = new Date();
     Seconds_ratio = Time.getSeconds() / 60;
     Minutes_ratio = (Seconds_ratio + Time.getMinutes()) / 60;
@@ -16,8 +16,29 @@ function Clock_fn(){
     Set_rotation(Hours_hand, Hours_ratio)
 }
 
-function Set_rotation(Eliment,Rotation_ratio){
+function Set_rotation(Eliment, Rotation_ratio) {
     Eliment.style.setProperty('--rotation', Rotation_ratio * 360)
 }
+let Full_area = document.querySelector(".Clock_container");
 
+function Auto_ratio_set(Full_area) {
+    let Content_ratio;
+    let Win_height = window.innerHeight;
+    let Win_width = window.innerWidth;
+    console.log("Win_hei is:- " + Win_height + ", Win_width is:- " + Win_width);
+    if (Win_height < Win_width) {
+        Content_ratio = Win_height;
+    } else {
+        Content_ratio = Win_width;
+    }
+    console.log("Content_ratio:- " + Content_ratio)
+    //SET PROPERTY VALUE
+    // Full_area = `${Content_ratio + "px"}`;
+    Full_area.style.cssText = `width:${Content_ratio + "px"};`
+}
+Auto_ratio_set(Full_area);
+onreset = ()=>{
+    Auto_ratio_set(Full_area)
+};
 Clock_fn()
+
